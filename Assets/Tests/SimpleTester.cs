7ea5093961bb45d33a39c23c7400e011e2f29a7f
dr-matt-smith <matt.smith@itb.ie>
2020-08-17 10:34:23 +0100

    added files to project

---
+++ Assets/Tests/SimpleTester.cs
@@ -0,0 +1,25 @@
+﻿using NUnit.Framework;
+
+class SimpleTester
+{
+    [Test]
+    public void TestOnePlusOneEqualsTwo()
+    {
+        // Arrange
+        int n1 = 1;
+        int n2 = 1;
+        int expectedResult = 2;
+        
+        // Act
+        int result = n1 + n2;
+        
+        // Assert
+        Assert.AreEqual(expectedResult, result);
+    }
+    
+    [Test]
+    public void TestOnePlusOneEqualsTwoNoVariables()
+    {
+        Assert.AreEqual(2, 1 + 1);
+    }    
+} 
